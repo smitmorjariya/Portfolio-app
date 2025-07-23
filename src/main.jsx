3182af367c6761ed048cb4a69342c45e4bfbc325
SMIT MORJARIYA <er.smit.2004@gmail.com>
2025-07-23 12:44:12 +0530

    Added Vercel Analytics to main render

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SpeedInsights />
+    <Analytics />
     <App />
   </StrictMode>,
 )
